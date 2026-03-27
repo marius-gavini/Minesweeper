becc1552e81553357f85769c7a2c8d0dac0a80fe
arthur-georget <arthur.georget@laplateforme.io>
2026-03-27 10:12:55 +0100

    fix(menu): Removed unsetted difficulty

--- src/states/Menu.py
+++ src/states/Menu.py
@@ -7,7 +7,7 @@ class Menu(State):
     def __init__(self):
         self.__background = Surface((1300, 731))
         self.__background.fill(Color("#066895"))
-        self.__difficulties = ["Easy","Medium","Hard","1mp0ss1bl3"]
+        self.__difficulties = ["Easy","Medium","Hard"]
         self.__difficulties_colors = [(50,150,50),(50,50,150),(150,50,50),(100,50,50)]
         self.__difficulties_hover_colors = [(20,100,20),(20,20,100),(100,20,20),(50,20,20)]
         self.__difficulty_index = 1
@@ -33,7 +33,7 @@ class Menu(State):
                                     self._context.set_state("Game")                             
                                 case "Difficulty":
                                     self.__difficulty_index += 1
-                                    if self.__difficulty_index > 3:
+                                    if self.__difficulty_index > 2:
                                          self.__difficulty_index = 0
                                     self.__buttons[1] = Button("Difficulty",(500,524),(300,60),
                                                                text = self.__difficulties[self.__difficulty_index],
